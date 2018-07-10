b1331f3aa0b5755dbb9676ba64fc61dc21a3911b
Alyssa Atain <alyssaatain@alyssas-mbp.attlocal.net>
2018-07-10 16:12:28 -0700

    add userInfoPanel, productsDisplay, BottomNav, Slider, BackToTopPanel and Footer

---
+++ src/pages/home/BottomNavBar.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const BottomNavBar = props => (
+    <div id="bottomNavBar" className="amazon-colors">
+     BottomNavBar
+    </div>
+);
+
+export default BottomNavBar;
